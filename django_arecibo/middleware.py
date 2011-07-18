73c21591b55c6b2b60c4ae0ae43a26b4f4d24f72
Andy McKay <amckay@mozilla.com>
2011-07-18 10:53:36 -0700

    add in celery aware middleware

--- django_arecibo/middleware.py
+++ django_arecibo/middleware.py
@@ -1,19 +1,21 @@
+from django.conf import settings
 from django.http import Http404
 from wrapper import post
 from tasks import post as delayed_post
 
+def arecibo_post(method, request, exception):
+    if getattr(settings, 'ARECIBO_SERVER_URL', ''):
+        if isinstance(exception, Http404):
+            method(request, 404)
+        else:
+            method(request, 500)
+
 class AreciboMiddleware(object):
-    post = post
 
     def process_exception(self, request, exception):
-        """ This is middleware to process a request
-        and pass the value off to Arecibo. """
-        # we keep the 404 check in there case
-        if isinstance(exception, Http404):
-            self.post(request, 404)
-        else:
-            # do we need to be finer grained on the exception status?
-            self.post(request, 500)
+        return arecibo_post(post, request, exception)
 
-class AreciboMiddlewareCelery(AreciboMiddleware):
-    post = delayed_post
+class AreciboMiddlewareCelery(object):
+
+    def process_exception(self, request, exception):
+        return arecibo_post(delayed_post, request, exception)
